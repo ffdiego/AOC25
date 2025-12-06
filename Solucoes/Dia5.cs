ad8a5eef1620ced8c365e8de27b73274562a9612
Diego Fernandes <diegogkx@gmail.com>
2025-12-05 23:33:34 -0400

    primeira parte dia 5

---
+++ Solucoes/Dia5.cs
@@ -0,0 +1,57 @@
+﻿using AOC24.Solucoes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOC24.Solucoes;
+
+public class ListaIngredientes
+{
+    private readonly List<(long menor, long maior)> rangeIngredientesFrescos;
+    private readonly List<long> ingredientesAVerificar;
+    public ListaIngredientes(string input)
+    {
+        var inputs = input.Split("\n\n");
+        var listaRange = inputs[0];
+        var listaChecagem = inputs[1];
+
+        this.rangeIngredientesFrescos = listaRange
+            .Split("\n")
+            .Select(range => (long.Parse(range.Split("-")[0]), long.Parse(range.Split("-")[1])))
+            .ToList();
+        this.ingredientesAVerificar = listaChecagem
+            .Split("\n")
+            .Select(long.Parse)
+            .ToList();
+    }
+
+    public int QuantidadeFrescos()
+    {
+        int frescos = 0;
+
+        foreach (var ingrediente in ingredientesAVerificar)
+        {
+            if (rangeIngredientesFrescos.Any(range => (range.menor <= ingrediente && range.maior >= ingrediente)))
+            {
+                frescos++; 
+            }
+        }
+
+        return frescos;
+    }
+}
+
+public class Dia5 : ISolucionador
+{
+    public string SolucaoParte1(string input)
+    {
+        var listaIngredientes = new ListaIngredientes(input);
+
+        return listaIngredientes.QuantidadeFrescos().ToString();
+    }
+
+    public string SolucaoParte2(string input)
+    {
+        return "";
+    }
+}
